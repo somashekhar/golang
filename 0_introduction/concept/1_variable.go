47fc602523ac20292b98cbe93c66927227270985
Som <somashekharvb@gmail.com>
2018-09-14 21:31:10 +0530

    Update 1_variable.go

--- 0_introduction/concept/1_variable.go
+++ 0_introduction/concept/1_variable.go
@@ -3,24 +3,24 @@ package main
 /* import "fmt" */
 
 func main(){
-	var a int =2			/* Declare and initialize */
+  var a int =2			/* Declare and initialize */
 
-	var b int				/* Declare and initialize seperately*/
-	b=3
+  var b int				/* Declare and initialize seperately*/
+  b=3
 
-	var c=4					/*Skip the datatype */
+  var c=4					/*Skip the datatype */
 
 
-	type INT_TYPE int		/* Alising and using the alias as data type*/
-    var d INT_TYPE = 5
+  type INT_TYPE int		/* Alising and using the alias as data type*/
+  var d INT_TYPE = 5
 
-	e := "Hai"				/* short variable declaration */
+  e := "Hai"				/* short variable declaration */
 
-	f := new(string)		/* create a varible and get pointer to it*/
-	*f="Som"
+  f := new(string)		/* create a varible and get pointer to it*/
+  *f="Som"
 
-   _=a; _=b; _=c; _=d; _=e; _=f
+  _=a; _=b; _=c; _=d; _=e; _=f
 
-	/*Golang is lexically scoped using blocks*/
+  /*Golang is lexically scoped using blocks*/
 
 }
