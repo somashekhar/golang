ef609b6a9586761eefa8891c782b5286a5e03c90
somashekhar <somashekharvb@gmail.com>
2018-09-17 13:32:15 +0000

    variable declaration

--- 0_basics/concept/1_variable.go
+++ 0_basics/concept/1_variable.go
@@ -1,13 +1,13 @@
+// var variable_list optional_data_type
+
 package main
 
 /* import "fmt" */
 
 func main(){
 	var a int =2			/* Declare and initialize */
-
 	var b int				/* Declare and initialize seperately*/
 	b=3
-
 	var c=4					/*Skip the datatype */
 
 
@@ -15,11 +15,10 @@ func main(){
     var d INT_TYPE = 5
 
 	e := "Hai"				/* short variable declaration */
-
 	f := new(string)		/* create a varible and get pointer to it*/
 	*f="Som"
 
-   _=a; _=b; _=c; _=d; _=e; _=f
+    _=a; _=b; _=c; _=d; _=e; _=f
 
 	/*Golang is lexically scoped using blocks*/
 
