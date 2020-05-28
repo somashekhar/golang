5cdf4182d7202828784dd975c8a39dfc018b689c
Som <somashekharvb@gmail.com>
2020-05-28 10:46:02 +0530

    Update 1_arrays.go

--- basics/1_com_dtypes/concepts/1_arrays.go
+++ basics/1_com_dtypes/concepts/1_arrays.go
@@ -1,15 +1,15 @@
 /*  #declaration and initialization
-	var variable_name [SIZE] variable_type = [SIZE] variable_type {value1,value2,..}
+	var variable_name [SIZE]variable_type = [SIZE]variable_type {value1,value2,..}
 	variable_name[index] = value
 
-	var	variable_name = [SIZE] variable_type {value1, value2,..}
-	var variable_name = [] variable_type {value1, value2,..}
+	var	variable_name = [SIZE]variable_type {value1, value2,..}
+	var variable_name = []variable_type {value1, value2,..}
 
 	#acessing
 	#use normarl for loop
-		for(int i; i< len(variable_name);i++)
+		for(int i:=0; i< len(variable_name);i++)
 	#use for loop with range
-		for int i, x:=range variable_name{
+		for i, x:=range variable_name{
 			use i for index and x for value inside array
 		}
 */
@@ -19,8 +19,8 @@ import "fmt"
 
 func main(){
 
-  var y[5] int=[5]int{1,2,3,4,5}
-  var x[5] int; x[2]=5
+  var y [5]int=[5]int{1,2,3,4,5}
+  var x [5]int; x[2]=5
   z :=[...]int{1,2,3}
 
   for i, v :=range x{
